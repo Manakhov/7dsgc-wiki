b1b8cdb2682f80719193a65dfaaf264b8280cc41
Manakhov <manakhovsvyatoslav@gmail.com>
2021-08-26 15:09:17 +0300

    create only 1 query request

--- sdsgc_wiki/main/views.py
+++ sdsgc_wiki/main/views.py
@@ -5,24 +5,25 @@ from .forms import HeroesForm, PropertiesForm, FilterForm
 
 
 def all_heroes(request):
-    heroes = Heroes.objects.order_by('-rank', 'name')
     filter_form = FilterForm()
+    filter_dict = {}
     if 'add_filter' in request.POST:
         filter_form = FilterForm(request.POST)
         errors = filter_form.errors           # does not work without this line
         filter_cleaned_data = filter_form.cleaned_data
         for key in filter_cleaned_data.keys():
             if key == 'color':
-                heroes = heroes.filter(color__in=filter_cleaned_data[key])
+                filter_dict['color__in'] = filter_cleaned_data[key]
             if key == 'race':
-                heroes = heroes.filter(race__in=filter_cleaned_data[key])
+                filter_dict['race__in'] = filter_cleaned_data[key]
             if key == 'properties' and filter_cleaned_data[key]:
                 selected_properties = []
                 for prop in filter_cleaned_data[key]:
                     selected_properties.append(prop.name)
-                heroes = heroes.filter(properties__name__in=selected_properties).distinct()
+                filter_dict['properties__name__in'] = selected_properties
             if key == 'uniqueness':
-                heroes = heroes.filter(uniqueness__contains=filter_cleaned_data[key])
+                filter_dict['uniqueness__contains'] = filter_cleaned_data[key]
+    heroes = Heroes.objects.filter(**filter_dict).distinct().order_by('-rank', 'name')
     context = {'title': 'All heroes 7ds-gc',
                'filter_form': filter_form,
                'heroes': heroes,
