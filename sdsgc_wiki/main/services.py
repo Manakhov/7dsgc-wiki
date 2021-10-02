c6a14025a32b28f4314e73223388a10ff0e6a778
Manakhov <manakhovsvyatoslav@gmail.com>
2021-10-02 20:46:39 +0300

    create get_all_heroes, get_filtered_heroes services

--- sdsgc_wiki/main/services.py
+++ sdsgc_wiki/main/services.py
@@ -10,6 +10,33 @@ def get_one_hero(pk):
     return hero
 
 
+def get_all_heroes():
+    """Getting all heroes from Heroes database"""
+    heroes = Heroes.objects.order_by('-rank', 'name')
+    return heroes
+
+
+def get_filtered_heroes(filter_form):
+    """Getting filtered heroes from Heroes database according to filter_form"""
+    filter_dict = {}
+    errors = filter_form.errors  # does not work without this line
+    filter_cleaned_data = filter_form.cleaned_data
+    for key in filter_cleaned_data.keys():
+        if key == 'color':
+            filter_dict['color__in'] = filter_cleaned_data[key]
+        if key == 'race':
+            filter_dict['race__in'] = filter_cleaned_data[key]
+        if key == 'properties' and filter_cleaned_data[key]:
+            selected_properties = []
+            for prop in filter_cleaned_data[key]:
+                selected_properties.append(prop.name)
+            filter_dict['properties__name__in'] = selected_properties
+        if key == 'uniqueness':
+            filter_dict['uniqueness__contains'] = filter_cleaned_data[key]
+    heroes = Heroes.objects.filter(**filter_dict).distinct().order_by('-rank', 'name')
+    return heroes
+
+
 def add_hero(request):
     """Adding hero in Heroes database"""
     heroes_form = HeroesForm(request.POST)
--- sdsgc_wiki/main/views.py
+++ sdsgc_wiki/main/views.py
@@ -2,29 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Heroes
 from .forms import HeroesForm, PropertiesForm, FilterForm
-from .services import get_one_hero, add_hero, add_property
+from .services import get_one_hero, get_all_heroes, get_filtered_heroes, add_hero, add_property
 
 
 def all_heroes(request):
-    filter_form = FilterForm()
-    filter_dict = {}
     if 'add_filter' in request.POST:
         filter_form = FilterForm(request.POST)
-        errors = filter_form.errors           # does not work without this line
-        filter_cleaned_data = filter_form.cleaned_data
-        for key in filter_cleaned_data.keys():
-            if key == 'color':
-                filter_dict['color__in'] = filter_cleaned_data[key]
-            if key == 'race':
-                filter_dict['race__in'] = filter_cleaned_data[key]
-            if key == 'properties' and filter_cleaned_data[key]:
-                selected_properties = []
-                for prop in filter_cleaned_data[key]:
-                    selected_properties.append(prop.name)
-                filter_dict['properties__name__in'] = selected_properties
-            if key == 'uniqueness':
-                filter_dict['uniqueness__contains'] = filter_cleaned_data[key]
-    heroes = Heroes.objects.filter(**filter_dict).distinct().order_by('-rank', 'name')
+        heroes = get_filtered_heroes(filter_form)
+    else:
+        filter_form = FilterForm()
+        heroes = get_all_heroes()
     context = {'title': 'All heroes 7ds-gc',
                'filter_form': filter_form,
                'heroes': heroes,
