0a6c673eb070c39b487bc36424f7ec65cb7b4f17
Manakhov <manakhovsvyatoslav@gmail.com>
2021-10-12 19:11:52 +0300

    rename create -> create_hero

--- sdsgc_wiki/main/urls.py
+++ sdsgc_wiki/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_heroes, name='all_heroes'),
     path('hero_<int:pk>', views.one_hero, name='one_hero'),
-    path('create', views.create_hero, name='create_hero'),
+    path('create_hero', views.create_hero, name='create_hero'),
     path('update_hero_<int:pk>', views.update_hero, name='update_hero'),
 ]
