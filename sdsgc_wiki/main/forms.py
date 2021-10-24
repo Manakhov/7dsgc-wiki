a8dbd8ffe4b924934ff41ef09a9874e5124409ab
Manakhov <manakhovsvyatoslav@gmail.com>
2021-10-24 20:21:22 +0300

    forms.py syntax fixes

--- sdsgc_wiki/main/forms.py
+++ sdsgc_wiki/main/forms.py
@@ -7,24 +7,25 @@ from django.contrib.auth.models import User
 class HeroesForm(ModelForm):
     class Meta:
         model = Heroes
-        fields = ['name',
-                  'icon',
-                  'rank',
-                  'color',
-                  'race',
-                  'uniqueness',
-                  'properties',
-                  ]
+        fields = [
+            'name',
+            'icon',
+            'rank',
+            'color',
+            'race',
+            'uniqueness',
+            'properties',
+        ]
         widgets = {
             'name': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите имя',
-                'autocomplete': 'off'
+                'autocomplete': 'off',
             }),
             'icon': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Добавьте адрес иконки',
-                'autocomplete': 'off'
+                'autocomplete': 'off',
             }),
             'rank': Select(attrs={
                 'class': 'form-control',
@@ -37,7 +38,7 @@ class HeroesForm(ModelForm):
             }),
             'uniqueness': Textarea(attrs={
                 'class': 'form-control',
-                'placeholder': 'Введите Уникальность'
+                'placeholder': 'Введите Уникальность',
             }),
             'properties': CheckboxSelectMultiple()
         }
@@ -51,27 +52,29 @@ class PropertiesForm(ModelForm):
             'name': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите новое свойство',
-                'autocomplete': 'off'
+                'autocomplete': 'off',
             })
         }
 
 
 class FilterForm(Form, ModelForm):
-    color = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=[('Зеленый', 'Зеленый'),
-                                                                        ('Красный', 'Красный'),
-                                                                        ('Синий', 'Синий')
-                                                                        ])
-    race = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=[('Богиня', 'Богиня'),
-                                                                       ('Великан', 'Великан'),
-                                                                       ('Демон', 'Демон'),
-                                                                       ('Неизвестно', 'Неизвестно'),
-                                                                       ('Фея', 'Фея'),
-                                                                       ('Человек', 'Человек')
-                                                                       ])
+    color = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=[
+        ('Зеленый', 'Зеленый'),
+        ('Красный', 'Красный'),
+        ('Синий', 'Синий'),
+    ])
+    race = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=[
+        ('Богиня', 'Богиня'),
+        ('Великан', 'Великан'),
+        ('Демон', 'Демон'),
+        ('Неизвестно', 'Неизвестно'),
+        ('Фея', 'Фея'),
+        ('Человек', 'Человек'),
+    ])
     uniqueness = CharField(required=False, widget=TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'Уникальность',
-        'autocomplete': 'off'
+        'autocomplete': 'off',
     }))
 
     class Meta:
@@ -83,16 +86,19 @@ class FilterForm(Form, ModelForm):
 
 
 class UserForm(Form, ModelForm):
-    password = CharField(widget=PasswordInput(attrs={'class': 'form-control',
-                                                     'placeholder': 'Введите пароль',
-                                                     'autocomplete': 'off'
-                                                     }))
+    password = CharField(widget=PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите пароль',
+        'autocomplete': 'off',
+    }))
 
     class Meta:
         model = User
         fields = ['username']
-        widgets = {'username': TextInput(attrs={'class': 'form-control',
-                                                'placeholder': 'Введите логин',
-                                                'autocomplete': 'off',
-                                                })
-                   }
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите логин',
+                'autocomplete': 'off',
+            })
+        }
